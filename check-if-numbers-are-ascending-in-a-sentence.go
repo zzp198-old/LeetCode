fc25e1082a9f8486aaf6fc397877cbb40f15229a
zzp198 <zzp198@163.com>
2023-01-03 18:47:43 +0800

    2023.1.3

---
+++ check-if-numbers-are-ascending-in-a-sentence.go
@@ -0,0 +1,20 @@
+package LeetCode
+
+import (
+	"strconv"
+	"strings"
+)
+
+func areNumbersAscending(s string) bool {
+	pre := 0
+	for _, key := range strings.Split(s, " ") {
+		if key[0] <= '9' {
+			v, _ := strconv.Atoi(key)
+			if pre >= v {
+				return false
+			}
+			pre = v
+		}
+	}
+	return true
+}
