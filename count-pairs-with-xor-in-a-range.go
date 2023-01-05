44d9ee3aaaee2b4d9fac10f2ddcb01d2e4695114
zzp198 <zzp198@163.com>
2023-01-05 17:08:18 +0800

    2023.1.5

---
+++ count-pairs-with-xor-in-a-range.go
@@ -0,0 +1,34 @@
+package LeetCode
+
+//	func countPairs(nums []int, low int, high int) (answer int) {
+//		for i := 0; i < len(nums); i++ {
+//			for j := i + 1; j < len(nums); j++ {
+//				xor := nums[i] ^ nums[j]
+//				if xor >= low && xor <= high {
+//					answer++
+//				}
+//			}
+//		}
+//		return
+//	}
+func countPairs(nums []int, low, high int) (ans int) {
+	cnt := map[int]int{}
+	for _, x := range nums {
+		cnt[x]++
+	}
+	for high++; high > 0; high >>= 1 {
+		nxt := map[int]int{}
+		for x, c := range cnt {
+			if high&1 > 0 {
+				ans += c * cnt[x^(high-1)]
+			}
+			if low&1 > 0 {
+				ans -= c * cnt[x^(low-1)]
+			}
+			nxt[x>>1] += c
+		}
+		cnt = nxt
+		low >>= 1
+	}
+	return ans / 2
+}
