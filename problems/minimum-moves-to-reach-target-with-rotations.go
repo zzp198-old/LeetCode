d294e4fbe04f72a11e06818615fe421bbbff5ba7
zzp198 <zzp198@163.com>
2023-02-05 11:59:34 +0800

    2023.2.5

---
+++ problems/minimum-moves-to-reach-target-with-rotations.go
@@ -0,0 +1,34 @@
+package problems
+
+type tuple struct{ x, y, s int }
+
+var dirs = []tuple{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}
+
+func minimumMoves(g [][]int) int {
+	n := len(g)
+	vis := make([][][2]bool, n)
+	for i := range vis {
+		vis[i] = make([][2]bool, n)
+	}
+	vis[0][0][0] = true // 初始位置
+	q := []tuple{{}}
+	for step := 1; len(q) > 0; step++ {
+		tmp := q
+		q = nil
+		for _, t := range tmp {
+			for _, d := range dirs {
+				x, y, s := t.x+d.x, t.y+d.y, t.s^d.s
+				x2, y2 := x+s, y+(s^1) // 蛇头
+				if x2 < n && y2 < n && !vis[x][y][s] &&
+					g[x][y] == 0 && g[x2][y2] == 0 && (d.s == 0 || g[x+1][y+1] == 0) {
+					if x == n-1 && y == n-2 { // 此时蛇头一定在 (n-1,n-1)
+						return step
+					}
+					vis[x][y][s] = true
+					q = append(q, tuple{x, y, s})
+				}
+			}
+		}
+	}
+	return -1
+}
