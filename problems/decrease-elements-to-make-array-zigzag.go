4f5e62a2e2a11fbf661b43a9d352636c25a03012
zzp198 <zzp198@163.com>
2023-02-27 10:01:51 +0800

    2023.2.27

---
+++ problems/decrease-elements-to-make-array-zigzag.go
@@ -0,0 +1,31 @@
+package problems
+
+import "math"
+
+func movesToMakeZigzag(nums []int) int {
+	s := [2]int{}
+	for i, x := range nums {
+		left, right := math.MaxInt, math.MaxInt
+		if i > 0 {
+			left = nums[i-1]
+		}
+		if i < len(nums)-1 {
+			right = nums[i+1]
+		}
+		s[i%2] += max(x-min(left, right)+1, 0)
+	}
+	return min(s[0], s[1])
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
